e7fd65445548ac47ae6c5d2c601486224f681566
tamjidka <106441810+tamjidka@users.noreply.github.com>
2022-05-28 22:58:10 -0400

    Create The-Minion-Game.py

---
+++ Python/Strings/The-Minion-Game.py
@@ -0,0 +1,26 @@
+#LINK: https://www.hackerrank.com/challenges/the-minion-game/problem
+#Language: Python
+#Difficulty: Medium
+
+
+def minion_game(string):
+    string_len = len(string)
+    const = vowel = 0
+    
+    for i in range(string_len):
+        if s[i] in 'AEIOU':
+            vowel = vowel + (string_len-i)
+        else:
+            const = const + (string_len-i)
+            
+    if const > vowel:
+        print('Stuart', const)
+    elif vowel > const:
+        print ('Kevin', vowel)
+    else:
+        print('Draw')
+            
+    
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
