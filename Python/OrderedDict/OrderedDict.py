ae9e6ba1acdf454c2fcd712c56f8833f78ad59d6
Tamjid K <106441810+tamjidka@users.noreply.github.com>
2022-09-21 22:42:32 -0400

    Solutions to collections.OrderedDict Task 1

---
+++ Python/OrderedDict/OrderedDict.py
@@ -0,0 +1,19 @@
+
+#LINK: https://www.hackerrank.com/challenges/py-collections-ordereddict/
+#Language: Python
+#Difficulty: Easy
+
+from collections import OrderedDict
+
+od = OrderedDict()
+
+for i in range(int(input())):
+    item_name, blank, price = input().rpartition(' ')
+    
+    if item_name not in od:
+        od[item_name] = int(price)
+    else:
+        od[item_name] += int(price)
+        
+for i in od.items():
+    print(*i)
