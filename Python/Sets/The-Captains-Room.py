b14c40ebdc4a334d4a19ca4bb7e8e317039f1bda
Tamjid K <106441810+tamjidka@users.noreply.github.com>
2022-09-23 22:36:43 -0400

    Solutions to The Captain's Room

---
+++ Python/Sets/The-Captains-Room.py
@@ -0,0 +1,20 @@
+#LINK: https://www.hackerrank.com/challenges/py-the-captains-room
+#Language: Python
+#Difficulty: Easy
+
+
+k = int(input())
+
+rooms = list(map(int, input().split()))
+
+a = set()
+b = set()
+
+for room in rooms:
+    if room not in a:
+        a.add(room)
+        b.add(room)
+    else:
+        b.discard(room)
+b = list(b)
+print(b[0])
